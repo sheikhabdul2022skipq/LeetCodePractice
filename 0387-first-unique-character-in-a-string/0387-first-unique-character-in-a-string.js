1cbac65dd039fc5f534ebfd21f906b2068459034
sheikhabdul2022skipq <113029233+sheikhabdul2022skipq@users.noreply.github.com>
2022-10-20 23:24:06 +0500

    Time: 245 ms (11.33%), Space: 49.9 MB (12.02%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    
+    let res = 0 ;
+    let splitArr = s.split("");
+    let setStr = new Set(s)
+    console.log(setStr)
+    for(let i = 0 ;i<s.length;i++){
+        if(s.indexOf(s[i])==s.lastIndexOf(s[i])){
+                return i ;
+            }
+    }
+    return -1 ;
+    // console.log(setStr.size)
+};
